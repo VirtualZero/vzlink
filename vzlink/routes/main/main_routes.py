63af92a1d51bda60d8add08ad49e22d52c3eba32
Birtchum <birtchum@virtualzero.net>
2019-07-17 17:56:04 -0400

    Finished redirect route

--- vzlink/routes/main/main_routes.py
+++ vzlink/routes/main/main_routes.py
@@ -1,1 +1,12 @@
-from flask import request, abort
+from flask import redirect
+from vzlink import app, db
+from vzlink.models.link import Link
+
+
+@app.route('/<hash_id>')
+def redirect_to_long_link(hash_id):
+    long_link = Link.query.filter_by(
+        hash_id=hash_id
+    ).first_or_404().long_link
+
+    return redirect(long_link)
